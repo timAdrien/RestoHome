9f8322097f94f9c4681a227851e9868c2f09533f
Blackro <melvinfelixpons@hotmail.fr>
2017-05-16 17:19:45 +0200

    “app”

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
 
-  root 'widgets#index'
+  root 'welcome#index'
   get 'widgets' => 'widgets#index'
 
   # Example of regular route:
